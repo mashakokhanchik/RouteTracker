de838040d06e9cd2b27bb9c0f117a67d8509ba17
Masha Kokhanchik <masha.kokhanchik@gmail.com>
2021-11-05 13:00:29 +0300

    Created a project with a framework Google Maps

--- RouteTracker/RouteTracker/App/AppDelegate.swift
+++ RouteTracker/RouteTracker/App/AppDelegate.swift
@@ -6,14 +6,16 @@
 //
 
 import UIKit
+import GoogleMaps
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        /// Настройка ключа GoogleMaps
+        GMSServices.provideAPIKey("AIzaSyALLRQ-0dC6d62DfXYBuLHI-PvVSWcFE-A")
         return true
     }
 
